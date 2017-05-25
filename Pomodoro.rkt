ed025fb763fbc406bc0a414d1cf702624bd76628
Bruce Forkush <reclusebruce@gmail.com>
2017-05-24 19:43:18 -0700

    update after Sazi code review

--- Pomodoro.rkt
+++ Pomodoro.rkt
@@ -9,18 +9,18 @@
 ;; =================
 ;; Constants:
 
-(define WIDTH 200)
-(define HEIGHT 200)
+(define WIDTH 150)
+(define HEIGHT 150)
 (define MTS (empty-scene WIDTH HEIGHT))
 (define FONT-SIZE 48)
 (define FOCUS-FONT-COLOR "red")
 (define BREAK-FONT-COLOR "blue")
-(define FOCUS-START-TIME 25)
-(define BREAK-START-TIME 5)
+(define FOCUS-START-MINUTES 25)
+(define BREAK-START-MINUTES 5)
 (define TIME-X-POS (/ WIDTH 2))
-(define TIME-Y-POS 150)
+(define TIME-Y-POS (* HEIGHT .7))
 (define TITLE-X-POS (/ WIDTH 2))
-(define TITLE-Y-POS 50)
+(define TITLE-Y-POS (* HEIGHT .3))
 (define FOCUS-TEXT "Focus")
 (define BREAK-TEXT "Break")
 
@@ -29,14 +29,18 @@
 
 (define-struct clock (min sec mode))
 ;; Clock is (make-clock Natural Natural Boolean)
-;; interp. a countdown clock showing minutes and seconds remaining, in the Focus or Break mode
-(define C1 (make-clock 25 0 false))
-(define C2 (make-clock 4 30 true))
+;; interp. the minutes and seconds remaining on a countdown clock, true mode means it is in 25 min "Focus" mode, false mode means it is in 5 min "Break" mode
+
+(define C1 (make-clock 25 0 true))
+(define C2 (make-clock 4 30 false))
 #;
 (define (fn-for-clock c)
-  (... (clock-min c)        ;Natural
-       (clock-sec c)        ;Natural
-       (clock-mode c)))     ;Boolean    
+  (... (clock-min  c)       ;Natural
+       (clock-sec  c)       ;Natural
+       (clock-mode c)))     ;Boolean
+
+;; Template rules used:
+;;  - compound: 3 fields
 
 ;; =================
 ;; Functions:
@@ -52,29 +56,33 @@
 
 ;; Clock -> Clock
 ;; produce the next lowest clock time by one second, changing modes and resetting after it reaches 0.
-(check-expect (next-clock (make-clock 25  0  true)) (make-clock 24 59 true))
-(check-expect (next-clock (make-clock 24 59  true)) (make-clock 24 58 true))
-(check-expect (next-clock (make-clock  4 30 false)) (make-clock  4 29 false))
-(check-expect (next-clock (make-clock  0 59  true)) (make-clock  0 58  true))
-(check-expect (next-clock (make-clock  0  0  true)) (make-clock  5  0 false))  ;Focus clock reaches 0, converts to Break clock
-(check-expect (next-clock (make-clock  0  0 false)) (make-clock 25  0 true))   ;Break clock reaches 0, converts to Focus clock
+(check-expect (next-clock (make-clock 25  0  true)) (make-clock                   24 59 true))
+(check-expect (next-clock (make-clock 24 59  true)) (make-clock                   24 58 true))
+(check-expect (next-clock (make-clock  4 30 false)) (make-clock                    4 29 false))
+(check-expect (next-clock (make-clock  0 59  true)) (make-clock                    0 58  true))
+(check-expect (next-clock (make-clock  0  0  true)) (make-clock BREAK-START-MINUTES   0 false))  ;Focus clock reaches 0, converts to Break clock
+(check-expect (next-clock (make-clock  0  0 false)) (make-clock FOCUS-START-MINUTES   0 true))   ;Break clock reaches 0, converts to Focus clock
+
 ;(define (next-clock c ) c)    ;stub
+; Template from Clock
 
 (define (next-clock c)
   (cond [(> (clock-sec c) 0)                           (make-clock (clock-min c) (- (clock-sec c) 1) (clock-mode c))]
         [(and (> (clock-min c) 0) (= (clock-sec c) 0)) (make-clock (- (clock-min c) 1) 59 (clock-mode c))]
-        [else                                          (make-clock (focus-mode? c) 0 (not (clock-mode c)))]))
+        [else                                          (make-clock (set-start-time c) 0 (not (clock-mode c)))]))
 
 ;; Clock -> Natural
-;; produces 5 if mode is true, 25 if mode is false
-(check-expect (focus-mode? (make-clock 10 10 true))   5)
-(check-expect (focus-mode? (make-clock 10 10 false)) 25)
-;(define (focus-mode? c) 0)     ;stub
+;; produces BREAK-START-MINUTES if mode is true, FOCUS-START-MINUTES if mode is false
+(check-expect (set-start-time (make-clock 10 10 true))  BREAK-START-MINUTES)
+(check-expect (set-start-time (make-clock 10 10 false)) FOCUS-START-MINUTES)
 
-(define (focus-mode? c)
+;(define (set-start-time c) 0)     ;stub
+; Template from Clock
+
+(define (set-start-time c)
   (if (clock-mode c)
-      BREAK-START-TIME
-      FOCUS-START-TIME))
+      BREAK-START-MINUTES
+      FOCUS-START-MINUTES))
 
 ;; Clock -> Image
 ;; render the appropriate clock time, title, and color
@@ -91,56 +99,79 @@
               (place-image (text BREAK-TEXT FONT-SIZE BREAK-FONT-COLOR) TITLE-X-POS TITLE-Y-POS
                            (place-image (text  (string-append  (number->string 4) ":" (number->string 30)) FONT-SIZE BREAK-FONT-COLOR)
                                         TIME-X-POS TIME-Y-POS MTS)))
+
 ;(define (render c) MTS)     ;stub
+; Template from Clock
 
 (define (render c)
   (place-image
-   (text (focus-text? c) FONT-SIZE (focus-color? c)) TITLE-X-POS TITLE-Y-POS
-   (place-image
-    (text  (string-append  (number->string (clock-min c)) ":" (need-padding? c) (number->string (clock-sec c))) FONT-SIZE (focus-color? c))
-    TIME-X-POS TIME-Y-POS MTS)))
+   (text (set-title c) FONT-SIZE (set-font-color c)) TITLE-X-POS TITLE-Y-POS
+   (render-clock c)))
+
+;; Clock -> Image
+;; render the clock with the appropriate time and color
+(check-expect (render-clock (make-clock 24 30 true)) (place-image (text  (string-append  (number->string 24) ":" (number->string 30)) FONT-SIZE FOCUS-FONT-COLOR)
+                                        TIME-X-POS TIME-Y-POS MTS))
+(check-expect (render-clock (make-clock 4 30 false)) (place-image (text  (string-append  (number->string 4) ":" (number->string 30)) FONT-SIZE BREAK-FONT-COLOR)
+                                        TIME-X-POS TIME-Y-POS MTS))
+
+;(define (render-clock c) MTS)     ;stub
+; Template from Clock
+
+(define (render-clock c)
+  (place-image
+    (text  (string-append  (number->string (clock-min c)) ":" (zero-padding c) (number->string (clock-sec c))) FONT-SIZE (set-font-color c))
+    TIME-X-POS TIME-Y-POS MTS))
 
 ;; Clock -> String
-;; produce "Focus" if true, "Break" if false
-(check-expect (focus-text? (make-clock 1 1 true))  FOCUS-TEXT)
-(check-expect (focus-text? (make-clock 1 1 false)) BREAK-TEXT)
-;(define (focus-text? c) "")    ;stub
+;; produce FOCUS-TEXT if true, BREAK-TEXT if false
+(check-expect (set-title (make-clock 1 1 true))  FOCUS-TEXT)
+(check-expect (set-title (make-clock 1 1 false)) BREAK-TEXT)
 
-(define (focus-text? c)
+;(define (set-title c) "")    ;stub
+; Template from Clock
+
+(define (set-title c)
   (if (clock-mode c)
       FOCUS-TEXT
       BREAK-TEXT))
 
 ;; Clock -> String
-;; produce "red" if true, "blue" if false
-(check-expect (focus-color? (make-clock 1 1 true))  "red")
-(check-expect (focus-color? (make-clock 1 1 false)) "blue")
-;(define (focus-color? c) "green")    ;stub
+;; produce FOCUS-FONT-COLOR if true, BREAK-FONT-COLOR if false
+(check-expect (set-font-color (make-clock 1 1 true))  FOCUS-FONT-COLOR)
+(check-expect (set-font-color (make-clock 1 1 false)) BREAK-FONT-COLOR)
 
-(define (focus-color? c)
+;(define (set-font-color c) "green")    ;stub
+; Template from Clock
+
+(define (set-font-color c)
   (if (clock-mode c)
       FOCUS-FONT-COLOR
       BREAK-FONT-COLOR))
 
 ;; Clock -> String
 ;; produce "0" if true, "" if false
-(check-expect (need-padding? (make-clock 1 10 true)) "")
-(check-expect (need-padding? (make-clock 1 9 true)) "0")
-;(define (need-padding? c) "x")    ;stub
+(check-expect (zero-padding (make-clock 1 10 true)) "")
+(check-expect (zero-padding (make-clock 1 9 true)) "0")
 
-(define (need-padding? c)
+;(define (zero-padding c) "x")    ;stub
+; Template from Clock
+
+(define (zero-padding c)
   (if (< (clock-sec c) 10)
       "0"
       "")) 
 
 ;; Clock KeyEvent -> Clock
 ;; changes clock mode between Focus and Break and resets clocks appropriately when space bar is pressed
-(check-expect (handle-key (make-clock 1 1 true)  " ") (make-clock  5 0 false))
-(check-expect (handle-key (make-clock 1 1 false) "x") (make-clock  1 1 false))
-(check-expect (handle-key (make-clock 1 1 false) " ") (make-clock 25 0 true))
-(check-expect (handle-key (make-clock 1 1 true)  "x") (make-clock  1 1 true))
+(check-expect (handle-key (make-clock 1 1 true)  " ") (make-clock  BREAK-START-MINUTES 0 false))
+(check-expect (handle-key (make-clock 1 1 false) "x") (make-clock                    1 1 false))
+(check-expect (handle-key (make-clock 1 1 false) " ") (make-clock FOCUS-START-MINUTES  0 true))
+(check-expect (handle-key (make-clock 1 1 true)  "x") (make-clock                    1 1 true))
+
 ;(define (handle-key c ke)c)    ;stub
+; Template according to KeyEvent
 
 (define (handle-key c ke)
-  (cond [(key=? ke " ") (make-clock (focus-mode? c) 0 (not (clock-mode c)))]
+  (cond [(key=? ke " ") (make-clock (set-start-time c) 0 (not (clock-mode c)))]
         [else c]))
